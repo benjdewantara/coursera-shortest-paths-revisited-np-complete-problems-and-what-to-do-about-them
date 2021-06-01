3e43b10ea8909d1210869713f4b25890701f3e72
Benyamin Manullang <benjdewantara@gmail.com>
2021-06-01 09:06:56 +0700

    Week 1 No 1 Checking negative cycle existence

--- week1/ShortestPath/BellmanFord.go
+++ week1/ShortestPath/BellmanFord.go
@@ -6,25 +6,28 @@ import (
 )
 
 type BellmanFord struct {
-	SourceVertex int
-	G            Graph.Graph
-	A            [][]float64
+	SourceVertex        int
+	G                   Graph.Graph
+	A                   [][]float64
+	NegativeCycleExists bool
 }
 
 func (b *BellmanFord) Evaluate() {
-	for numEdgeBudget := 1; numEdgeBudget <= b.G.NumVertices; numEdgeBudget++ {
+	for numEdgeBudget := 1; numEdgeBudget < len(b.A); numEdgeBudget++ {
 		for nodeIdx := 0; nodeIdx < b.G.NumVertices; nodeIdx++ {
 			b.A[numEdgeBudget][nodeIdx] = b.A[numEdgeBudget-1][nodeIdx]
 			minOfIncomingToNodeDest := float64(0)
 			nodeDest := nodeIdx + 1
 			for idx, edge := range b.G.EdgesGoingIntoNode(nodeDest) {
+				pathLength := b.A[numEdgeBudget-1][edge.Tail-1] + float64(edge.Weight)
+
 				if idx == 0 {
-					minOfIncomingToNodeDest = b.A[numEdgeBudget-1][edge.Tail-1] + float64(edge.Weight)
+					minOfIncomingToNodeDest = pathLength
 					continue
 				}
 
-				if b.A[numEdgeBudget-1][edge.Tail-1] < minOfIncomingToNodeDest {
-					minOfIncomingToNodeDest = b.A[numEdgeBudget-1][edge.Tail-1] + float64(edge.Weight)
+				if pathLength < minOfIncomingToNodeDest {
+					minOfIncomingToNodeDest = pathLength
 				}
 			}
 
@@ -33,6 +36,20 @@ func (b *BellmanFord) Evaluate() {
 			}
 		}
 	}
+
+	b.checkCycleExistence()
+}
+
+func (b *BellmanFord) checkCycleExistence() {
+	lastIndx := len(b.A) - 1
+	for i := 0; i < len(b.A[0]); i++ {
+		if b.A[lastIndx-1][i] != b.A[lastIndx][i] {
+			b.NegativeCycleExists = true
+			return
+		}
+	}
+
+	b.NegativeCycleExists = false
 }
 
 func InitBellmanFord(g Graph.Graph, sourceVertex int) BellmanFord {
--- week1/main.go
+++ week1/main.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	//g := Graph.ReadTextfile("_test_g1_cycle.txt")
+	//g := Graph.ReadTextfile("_test_g1.txt")
 	g := Graph.ReadTextfile("_6ff856efca965e8774eb18584754fd65_g1.txt")
 	bellmanFord := ShortestPath.InitBellmanFord(g, 1)
 	bellmanFord.Evaluate()
