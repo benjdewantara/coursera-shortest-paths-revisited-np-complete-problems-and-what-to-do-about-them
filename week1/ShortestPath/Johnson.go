9095970d3dcddd5f923ff5a7df587fcd271b4c70
Benyamin Manullang <benjdewantara@gmail.com>
2021-06-02 01:43:13 +0700

    Week 1 No 1 Initial Bellman-Ford execution of the Johnson algorithm

---
+++ week1/ShortestPath/Johnson.go
@@ -0,0 +1,54 @@
+package ShortestPath
+
+import (
+	"../Graph"
+)
+
+type Johnson struct {
+	G           Graph.Graph
+	GPrime      Graph.Graph
+	NodeExtra   int
+	BellmanFord BellmanFord
+}
+
+func InitJohnson(g Graph.Graph) Johnson {
+	gPrime := g
+	gPrime.Adj = append(gPrime.Adj, make([][2]int, g.NumVertices))
+	gPrime.NumVertices++
+
+	for nodeIdx := 0; nodeIdx < g.NumVertices; nodeIdx++ {
+		node := nodeIdx + 1
+		gPrime.Adj[len(gPrime.Adj)-1][nodeIdx] = [2]int{node, 0}
+	}
+
+	var j = Johnson{
+		G:         g,
+		GPrime:    gPrime,
+		NodeExtra: g.NumVertices + 1,
+	}
+
+	j.BellmanFord = InitBellmanFord(gPrime, j.NodeExtra)
+	j.BellmanFord.Evaluate()
+
+	if !j.BellmanFord.NegativeCycleExists {
+		for tailIdx := 0; tailIdx < j.G.NumVertices; tailIdx++ {
+			for headIdx := 0; headIdx < len(j.GPrime.Adj[tailIdx]); headIdx++ {
+				if j.GPrime.Adj[tailIdx] == nil {
+					continue
+				}
+
+				e := Graph.Edge{
+					Tail:   tailIdx + 1,
+					Head:   j.GPrime.Adj[tailIdx][headIdx][0],
+					Weight: j.GPrime.Adj[tailIdx][headIdx][1],
+				}
+
+				j.GPrime.Adj[tailIdx][headIdx][1] =
+					e.Weight +
+						int(j.BellmanFord.A[gPrime.NumVertices][tailIdx]-j.BellmanFord.A[gPrime.NumVertices][e.Head-1])
+			}
+		}
+	}
+
+	return j
+}
--- week1/main.go
+++ week1/main.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	//g := Graph.ReadTextfile("_test_g1_cycle.txt")
 	//g := Graph.ReadTextfile("_test_g1.txt")
-	g := Graph.ReadTextfile("_6ff856efca965e8774eb18584754fd65_g1.txt")
-	bellmanFord := ShortestPath.InitBellmanFord(g, 1)
-	bellmanFord.Evaluate()
+	g := Graph.ReadTextfile("_test_g1_negative_edge.txt")
+	//g := Graph.ReadTextfile("_6ff856efca965e8774eb18584754fd65_g1.txt")
+	johnson := ShortestPath.InitJohnson(g)
 
-	fmt.Println(bellmanFord.SourceVertex)
+	fmt.Println(johnson.G.NumVertices)
 }
