b75554d985eb1d1d7054f37aea583afbb9df5b71
Benyamin Manullang <benjdewantara@gmail.com>
2021-07-23 06:04:06 +0700

    Week 2 Still an unfeasible brute-force solution

--- week2/Graph/GraphCoordinate.go
+++ week2/Graph/GraphCoordinate.go
@@ -1,7 +1,9 @@
 package Graph
 
 import (
+	"fmt"
 	"io/ioutil"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -9,10 +11,124 @@ import (
 type GraphCoordinate struct {
 	Vertices         []int
 	VertexCoordinate [][2]float64
+	A                map[string]float64
+	MinDist          float64
+}
+
+func (g *GraphCoordinate) Length() int {
+	return len(g.VertexCoordinate)
+}
+
+func (g *GraphCoordinate) EvaluateTsp() {
+	var finalSubset []int
+
+	for m := 2; m <= g.Length(); m++ {
+		subsetGenerator := CombinationExceptFirstElem{
+			Elements: g.Vertices,
+			M:        m,
+		}
+
+		for _, subset := range subsetGenerator.GetCombinations() {
+			finalSubset = append([]int{}, subset...)
+			for jIdxInSubset := 1; jIdxInSubset < len(subset); jIdxInSubset++ {
+				vertexJInSubset := subset[jIdxInSubset]
+				g.putDistance(subset, vertexJInSubset)
+			}
+		}
+	}
+
+	minDist := -1.0
+	for i := 1; i < len(g.Vertices); i++ {
+		vertexSecondToLast := g.Vertices[i]
+		key := SubsetWVertexDestToString(finalSubset, vertexSecondToLast)
+
+		d := g.A[key] +
+			g.DistanceBetween(vertexSecondToLast, 1)
+		if minDist == -1 || d < minDist {
+			minDist = d
+		}
+	}
+
+	g.MinDist = minDist
+
+	fmt.Println(fmt.Sprintf("EvaluateTsp: finalSubset = %s", finalSubset))
+	fmt.Println(fmt.Sprintf("EvaluateTsp: MinDist = %f", g.MinDist))
+	fmt.Println("EvaluateTsp: End of function")
+}
+
+func (g *GraphCoordinate) DistanceBetween(vertexA int, vertexB int) float64 {
+	xA, yA := g.VertexCoordinate[vertexA-1][0], g.VertexCoordinate[vertexA-1][1]
+	xB, yB := g.VertexCoordinate[vertexB-1][0], g.VertexCoordinate[vertexB-1][1]
+	deltaX := xA - xB
+	deltaY := yA - yB
+	return math.Sqrt((deltaX * deltaX) + (deltaY * deltaY))
+}
+
+func (g *GraphCoordinate) putDistance(subset []int, vertexJInSubset int) float64 {
+	key := SubsetWVertexDestToString(subset, vertexJInSubset)
+	dist, exists := g.A[key]
+	if exists {
+		return dist
+	}
+
+	if len(subset) == 1 {
+		dist = math.Inf(1)
+		if subset[0] == 1 && vertexJInSubset == 1 {
+			dist = 0
+		}
+		g.A[key] = dist
+		return g.A[key]
+	} else if len(subset) == 2 {
+		dist = g.DistanceBetween(subset[0], subset[1])
+		g.A[key] = dist
+		return g.A[key]
+	}
+
+	minDist := -1.0
+	for kIdxInSubset := 1; kIdxInSubset < len(subset); kIdxInSubset++ {
+		vertexKInSubset := subset[kIdxInSubset]
+		if vertexKInSubset == vertexJInSubset {
+			continue
+		}
+
+		vertexDestInSubsetIdx := -1
+		for destIdxInS := 0; destIdxInS < len(subset); destIdxInS++ {
+			if subset[destIdxInS] == vertexJInSubset {
+				vertexDestInSubsetIdx = destIdxInS
+				break
+			}
+		}
+
+		subsetWoVertexDest := append([]int{}, subset[0:vertexDestInSubsetIdx]...)
+		subsetWoVertexDest = append(subsetWoVertexDest, subset[vertexDestInSubsetIdx+1:]...)
+
+		d := g.putDistance(subsetWoVertexDest, vertexKInSubset) +
+			g.DistanceBetween(vertexKInSubset, vertexJInSubset)
+		if minDist == -1 || d < minDist {
+			minDist = d
+		}
+	}
+
+	g.A[key] = minDist
+	return g.A[key]
+}
+
+func SubsetWVertexDestToString(subset []int, vertexDest int) string {
+	return fmt.Sprintf("%s>%d", IndicesToString(subset), vertexDest)
+}
+
+func IndicesToString(indices []int) string {
+	s := fmt.Sprintf("%d", indices[0])
+	for i := 1; i < len(indices); i++ {
+		s = fmt.Sprintf("%s,%d", s, indices[i])
+	}
+
+	return s
 }
 
 func ReadTextfile(filepath string) GraphCoordinate {
 	g := GraphCoordinate{}
+	g.A = make(map[string]float64)
 
 	contentBytes, _ := ioutil.ReadFile(filepath)
 	for lineIndx, intStr := range strings.Split(string(contentBytes), "\n") {
--- week2/main.go
+++ week2/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"./Graph"
-	"fmt"
 )
 
 func main() {
 	g := Graph.ReadTextfile("_f702b2a7b43c0d64707f7ab1b4394754_tsp.txt")
-	if g.VertexCoordinate != nil {
-		fmt.Println("Hell on earth")
-	}
+	//g := Graph.ReadTextfile("_test_tsp.txt")
+	g.EvaluateTsp()
 }
